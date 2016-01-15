59a26b0d2e29110115b1d59320ab590ea61b8804
Carlos Cuatin <cecg1996@gmail.com>
2016-01-15 16:46:13 -0500

    stlyes inlines

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router'
 
 class App extends React.Component {
   render() {
+    let style = {
+      app: {
+        color: 'red'
+      }
+    }
     return (
       <div>
-        <h1>App</h1>
-        <span>by Carlos Cuatin cecg1996@gmail.com</span>
+        <h1 style={style.app}>App</h1>
+        <span className="carlos">by Carlos Cuatin cecg1996@gmail.com</span>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
